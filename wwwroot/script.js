e1ca04d012e24f78da4a04ab87c4ea77367d1569
Rodrigo Guedes <guedero@cronos.be>
2024-06-07 14:05:34 +0200

    change log data

---
+++ wwwroot/script.js
@@ -0,0 +1,8 @@
+﻿function sendMessage() {
+    var userMessage = document.getElementById("userMessage").val();
+
+    $.post("/", { userMessage: userMessage }, function (data) {
+        $("#chatBox").append("<p>User: " + userMessage + "</p>");
+        $("#chatBox").append("<p>AI: " + data.response + "</p>");
+    });
+}
